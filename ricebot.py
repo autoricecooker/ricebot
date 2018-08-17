dcc87e409c75bdd33bcfb098e2ae6be5578aa8ce
autoricecooker <taerogar@gmail.com>
2018-08-17 13:54:49 +0800

    Debugging autoreply for werewolf gc

--- ricebot.py
+++ ricebot.py
@@ -23,7 +23,7 @@ def handle(msg):
 
 
 	if content_type == "text":
-  print(msg)
+		print(msg)
 		if chat_id == "234762812":
 			print("chat id is testGC")
 		if (any(x in msg["text"].lower() for x in sadgreet) and "ricecooker" in msg["text"].lower()):
