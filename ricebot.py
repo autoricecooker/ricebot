84263840e0427425b07c27e566c5173c86a7653f
autoricecooker <taerogar@gmail.com>
2018-09-06 10:30:41 +0800

    Removed username requirement for greetings autoreply

--- ricebot.py
+++ ricebot.py
@@ -32,19 +32,19 @@ def handle(msg):
 			time.sleep(1)
 			ricebot.sendMessage(chat_id, ">.>", parse_mode="Markdown")
 			ricebot.sendMessage(chat_id, "_spills rice_", parse_mode="Markdown")
-        #send WW GC invite link in main GC
+			#send WW GC invite link in main GC
 		elif (any(x in msg["text"] for x in werewolfcommands) and (chat_id == -1001043875036)):
 			ricebot.sendMessage(chat_id, "Hi, you may join this GC's werewolf game channel at https://t.me/joinchat/" + wwgc, parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
 		elif (any(x in msg["text"].lower() for x in assumptgreet)):
 			time.sleep(1)			
 			ricebot.sendMessage(chat_id, "Di ako yun", parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
-		#send unplug GIF if a kill greeting has been sent 
+			#send unplug GIF if a kill greeting has been sent 
 		elif (any(x in msg["text"].lower() for x in killgreet) and "ricecooker" in msg["text"].lower()): 
 			ricebot.sendDocument(chat_id, unpluggif)
-        #send reply based on parameters
+			#send reply based on parameters
         #elif ((msg["from"]["username"] is not None) and (msg["from"]["username"] != "riceCooker") and (msg["text"].lower() == "hi")):
-		elif ((msg["from"]["username"] != "riceCooker") and (msg["text"].lower() == "hi")):
-            #print (msg["from"]["username"])
+		elif (msg["text"].lower() == "hi" or msg["text"].lower() == "hi rice"):
+			#print (msg["from"]["username"])
 			time.sleep(1)
 			ricebot.sendMessage(chat_id, random.choice(randomgreet), parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
 
