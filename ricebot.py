70b562185318c8923678663658a672892b7e9b5e
autoricecooker <taerogar@gmail.com>
2018-09-12 10:36:38 +0800

    Disable telepot.glance. Insert content_type checker right into source.

--- ricebot.py
+++ ricebot.py
@@ -13,6 +13,7 @@ ricebot = telepot.Bot(os.environ["BOT_TOKEN"])
 wwgc = os.environ["WW_GC"]
 riceID = ricebot.getMe()
 ricecontent = 0
+all_content_types = ["text", "audio", "document", "game", "photo", "sticker", "video", "voice", "video_note","contact", "location", "venue", "new_chat_member", "left_chat_member", "new_chat_title","new_chat_photo",  "delete_chat_photo", "group_chat_created", "supergroup_chat_created","channel_chat_created", "migrate_to_chat_id", "migrate_from_chat_id", "pinned_message","new_chat_members", "invoice", "successful_payment"]
 sadgreet = ["spill", "trip", "eat", "bite", "tips", "tips over"]
 killgreet = ["unplug", "bang", "kill", "rip"]
 randomgreet = ["Hello", "Hi", "Greetings", "Good day", "How are ya?", "Yes", "No", "What's up?"]
@@ -22,20 +23,32 @@ unpluggif = "CgADBQADNwADtjzaDXzFsIuaINkHAg"
 landigif = ["CgADBQADHQAD6QuQV_mxPdwxj0s2Ag", "CgADBQADKQADvG2JVi4mEJDnylDvAg"]
 landichance = 0
 
-def handle(msg):
-	content_type, chat_type, chat_id, msg_date, msg_id = telepot.glance(msg, flavor="chat", long="True")
 
+#get content type
+def _find_first_key(d, keys):
+    for k in keys:
+        if k in d:
+            return k
+    raise KeyError("No suggested keys %s in %s" % (str(keys), str(d)))
+
+def handle(msg):
+	#content_type, chat_type, chat_id = telepot.glance(msg, flavor="chat", long="False")
+	content_type = _find_first_key(msg, all_content_types)
+	msg_id = msg["message_id"]
+	chat_id = msg["chat"]["id"]
 
 	if content_type == "text":
 		#gc specific autoreply
 
+		#check if GC is for testing
 		if chat_id == -234762812:
 			print("chat id is " + msg["chat"]["title"])
 			if (("reply_to_message" in msg) and (msg["text"].lower() == "landi mo") and (msg["from"]["id"] == msg["reply_to_message"]["from"]["id"])):
 				print("same user ID")
+		#check if GC is production
 		elif (chat_id == -1001043875036):
-			#give 25% chance for Jerome landigif, 75% chance for generic landigif
-			landichance = random.randint(1,4) % 4
+			#give 20% chance for Jerome landigif, 80% chance for generic landigif
+			landichance = random.randint(1,5) % 5
 			#autoreply for hipo messages
 			if (msg["text"].lower() == "hipo"):
 				if (landichance == 0):
@@ -58,8 +71,12 @@ def handle(msg):
 	
 		
 		#non-gc specific autoreply
+		#send greetings
+		if (msg["text"].lower() == "hi" or msg["text"].lower() == "hi rice"):
+			time.sleep(1)
+			ricebot.sendMessage(chat_id, random.choice(randomgreet), parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
 		#sad greetings autoreply
-		if (any(x in msg["text"].lower() for x in sadgreet) and "ricecooker" in msg["text"].lower()):
+		elif (any(x in msg["text"].lower() for x in sadgreet) and "ricecooker" in msg["text"].lower()):
 			ricebot.sendMessage(chat_id,"<.<", parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
 			time.sleep(1)
 			ricebot.sendMessage(chat_id, ">.>", parse_mode="Markdown")
@@ -67,13 +84,14 @@ def handle(msg):
 		#send unplug GIF if a kill greeting has been sent 
 		elif (any(x in msg["text"].lower() for x in killgreet) and "ricecooker" in msg["text"].lower()): 
 			ricebot.sendDocument(chat_id, unpluggif)		
-		#send greetings
-		elif (msg["text"].lower() == "hi" or msg["text"].lower() == "hi rice"):
-			time.sleep(1)
-			ricebot.sendMessage(chat_id, random.choice(randomgreet), parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
+		
+		
 
 
 MessageLoop(ricebot, handle).run_as_thread()
 
 while 1:
 	time.sleep(10)
+
+
+#Credits to Nick Lee for the telepot python framework for Telegram bot API
