41131d70f134dcbab1b17cb40fd5107372acd62c
autoricecooker <taerogar@gmail.com>
2018-09-26 18:42:54 +0800

    changed if to elif

--- ricebot.py
+++ ricebot.py
@@ -225,7 +225,7 @@ def handle(ricebot):
 		if (msg_text):
 			if (user_id == "339707076" and msg_text == "hi"):
 				ricebot.sendMessage(chat_id, "<code>Pass</code>", parse_mode="HTML")
-			if (angerychance and (msg_text == "hi" or msg_text == "hi rice")):
+			elif (angerychance and (msg_text == "hi" or msg_text == "hi rice")):
 				time.sleep(1)
 				ricebot.sendMessage(chat_id, random.choice(randomgreet), parse_mode="Markdown", disable_web_page_preview=None, disable_notification=True, reply_to_message_id=msg_id)
 			#sad greetings autoreply
