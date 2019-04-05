3511dc9fe01df1dae078dfe1bd6ba045fcfaaef2
Legaspi <r.legaspi@fujitsu.com>
2019-04-05 09:10:35 +0800

    Added new file for storing media variables.

--- ricebot.py
+++ ricebot.py
@@ -8,6 +8,7 @@ import random
 import telegram
 import telegram.ext
 from telegram.error import NetworkError, Unauthorized
+from mediadata import *
 from time import sleep
 from telegram.ext.dispatcher import run_async
 
@@ -17,48 +18,6 @@ from telegram.ext.dispatcher import run_async
 testGCID = int(os.environ["TEST_GC"])
 prodGCID = int(os.environ["PROD_GC"])
 cebGCID = int(os.environ["CEB_GC"])
-sadgreet = ["spill", "trip", "eat", "eat you", "bite", "bite you","tips", "tips over"]
-killgreet = ["unplug", "unplug you", "kill", "kill you", "rip", "destroy", "hate", "hate you"]
-randomgreet = ["Hello", "Hi", "Greetings", "Good day", "How are ya?", "Yes", "No", "What's up?"]
-assumptgreet = ["akala ko si rice", "akala ko si ricecooker", "akala ko si @ricecooker", "kala ko si rice", "kala ko si @ricecooker", "kala ko si ricecooker", "ikaw ba yan rice", "kaw ba yan rice", "rice ikaw ba yan", "rice kaw ba yan", "ikaw ba yan @ricecooker", "si rice ba yun", "si @ricecooker ba yun", "si rice ba un", "si @ricecooker ba un", "is that @ricecooker"]
-angerygreet = ["Shooo", "...", "Wag ka na", ":|"]
-athensgreet =["athens", "ganda ako", "ganda ko", "cute ako", "cute me", "cute kasi ako", "cute ko", "cute talaga ako","maganda talaga ako", "maganda kasi ako", "im beautiful", "i am beautiful", "i'm beautiful"]
-stressgreet = ["stress", "stresss", "stressss", "stressed", "stressssss", "overstress", "megastress"]
-blessgreet = ["bless", "blesss", "blessed"]
-drunkgreet = ["lasing"]
-werewolfcommands = ["/werewolf@riceCookerisnotAbot", "/startchaos@werewolfbot", "/nextgame@werewolfbot", "/start@werewolfbot",]
-atomgreet = ["atom", "nambabakod"]
-parrotgreet = ["party parrot", "partyparrot"]
-tikigreet = ["meaty", "thicc", "thick", "tiki"]
-ferdzgreet = ["happy", "genie"]
-jakegreet = ["happy", "joyful"]
-justgreet = ["hi", "hello", "henlo", "hallu", "elo", "hellu", "haller", "haler"]
-leigreetphrase = ["cute ako", "maganda ako", "pinaka cute", "pinaka maganda", "i am beautiful", "sexy ako", "most beautiful", "ganda ko", "5'11", "qt ko", "qt ako"]
-leigreetword = ["pinakacute", "cute", "pinakamaganda"]
-weebgreet = ["anime was a mistake", "weeb shit", "fucking weeb", "fuckin weeb", "fucking weeaboo", "fuckin weeaboo", "damn weeb", "damn weeaboo"]
-leisticker = ["CAADBQADIQEAAiO4mBCSjVKUWk7MNwI", "CAADBQADHAEAAiO4mBCeaC0LYAlkowI", "CAADBQADIAEAAiO4mBD3UdIBPEO6WwI", "CAADBQADGwEAAiO4mBC51PR572t9EgI", "CAADBQADHwEAAiO4mBBJWiwq_cjWdQI", "CAADBQADHgEAAiO4mBBMQuYcpcSX2AI"]
-#leigif = ["CgADBQADEAADS754Vj0cIPOA5fAWAg", "CgADBQADFQADDHrhVXLWPyi-fVESAg", "CgADBQADEQADNNrpV0yGyX3SyadcAg", "CgADBQADHwADYlzxVd_bBWXEyqHUAg"]
-leigif = ["CgADBQADEQADNNrpV_JyDVwU2d1rAg", "CgADBQADFQADDHrhVeaTS0bvbyRWAg", "CgADBQADEAADS754Vha1dmKytEvNAg", "CgADBQADHwADYlzxVfOzb9cCUkuqAg"]
-#stressgif = ["CgADBQADNgAD1k_RVO3goPd_-i6UAg", "CgADBQADeAADXqhQVB4H-vGgquQBAg", "CgADBQADagADIQNZVQwwt6F0hhTnAg", "CgADBQADMQADSQ5QVWEW6SjSG4KYAg"]
-stressgif = ["CgADBQADeAADXqhQVCksGu6IfbQMAg", "CgADBQADMQADSQ5QVUn-yBLRrDvBAg", "CgADBQADagADIQNZVdJEKrwDU24lAg", "CgADBQADNgAD1k_RVFSZibSpxmu4Ag", "CgADBQADFgADjGnIVUDk7DZAN-6XAg", "CgADBQADAwADXbeJVIa-vUgkTDSCAg"]
-blessgif =["CgADBQADQwADUt_RVJbf7QgSjFMQAg"]
-#genieferdzgif = "CgADBQADEgADUT_BVphVHpXa9x1UAg"
-genieferdzgif = "CgADBQADEgADUT_BVqvCsq_FYDT-Ag"
-#unpluggif = "CgADBQADNwADtjzaDem_pNtFz2CxAg"
-unpluggif = "CgADBQADNwADtjzaDXzFsIuaINkHAg"
-#landigif = ["CgADBQADKQADvG2JViq33V_DvoF9Ag", "CgADBQADHQAD6QuQVwZwVsITdN6hAg"]
-landigif = ["CgADBQADHQAD6QuQV_mxPdwxj0s2Ag", "CgADBQADKQADvG2JVi4mEJDnylDvAg"]
-louisegif = ["CgADBQADHAADVMqxV2iqwCWWKYXRAg", "CgADBQADRQADggs5VTtpVvYfIcqBAg", "CgADBQADGwADwI9BVYEBhY4qglXKAg", "CgADBQADWwADfhjQVy2VyBHiS3kWAg", "CgADBQADXQADlN5hVIkXruw21LEsAg"]
-toastiesgif = ["CgADBQADLwADv4dBVFVtZJ9jdSwFAg"]
-jakegif = ["CgADBQADBwADGIgpVpF8EgH6Mc-9Ag", "CgADBQADCgADuNkwVpThek7KAmXsAg"]
-drunkpic = ["AgADBQADOagxGxB_aVZNfo9wNUvyYvGl1jIABMOR93PzURnicEMDAAEC", "AgADBQADOqgxGxB_aVav5vKmhLduCGqk1jIABOQDiHpGpExCJUMDAAEC", "AgADBQADkagxG6Oe8FbPeySNTsva1S5U2zIABHbiq4wGrTTHhxABAAEC", "AgADBQADYKgxGzx76FYspQoUVI_orGFh2zIABPdENNUjOZ6HMQoBAAEC", "AgADBQADyagxG3vgQFfzWyPyZwABhDpToNYyAARIZLO6JHE6rwHyAwABAg", "AgADBQADyqgxG3vgQFdnwo6v0N37ltKu1jIABImnO1LEv_hLsvgDAAEC", "AgADBQADy6gxG3vgQFdhN5CqLNBoaZps3jIABIzvbj8OdXrYa0EAAgI", "AgADBQADzKgxG3vgQFd0wYPinyYN3Zhq2zIABNpbg4ad52tcN0oBAAEC"]
-parrotgif = ["CgADBQADbwADJAExVcp6mddA-OvtAg", "CgADBQADLQADkxKZVrE-Wx30uXiNAg", "CgADBQADWAADeL-YVnw3sjCV8aeLAg", "CgADBQADMAADkxKZVp3RsGgriq4TAg", "CgADBQADWQADeL-YVsV0J7Zx1H3-Ag", "CgADBQADLwADamEJVwABfmqsGruuIAI", "CgADBQADRQADxKcIVnp4XIZkwZhFAg", "CgADBQADRgADxKcIVtBU7hnUu1vtAg", "CgADBQADSgADFBMRVvS30XYL8EfxAg", "CgADBQADTQADxKcQVqmmDa3r6kUvAg"]
-ftwentygif = ["CgADBQADZwADC8sAAVdyPHOcrabSlAI", "CgADBQADBQADilkQVlcE7H79KrlZAg"]
-flandersgif = ["CgADBAADOQADvho0UUaTNwGy6fWJAg"]
-weebgif = ["CgADBQADOwADyE_wVe3mk7g8DJnwAg", "CgADBQADWAAD5XgIVd4wMCz9ESrCAg"]
-prayerpic = "AgADBQADbagxG4ocsVfyRzCWKWz83cBQ9jIABFnbxMn-gKxBdGgBAAEC"
-morningpic = "AgADBQADRKgxG_aNeFXkzaKcraJiFdOA3zIABKp6ugP3u2PoJjgBAAEC"
-weedjesusgif = "CgADBQADNgADZj7JVEzPU_TiD4UHAg"
 landichance = 0
 angerychance = 0
 atomchance = 0
