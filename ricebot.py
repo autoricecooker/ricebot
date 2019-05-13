70707a08473bfe01f0e6a6bce5db0da94be4f4b4
autoricecooker <taerogar@gmail.com>
2019-05-13 20:09:17 +0800

    Attempt to print out new member name in testGC.

--- ricebot.py
+++ ricebot.py
@@ -144,6 +144,7 @@ def testGChandle(update: telegram.Update, context: telegram.ext.CallbackContext)
 		#Send motd when new members are added
 		if (update.message.new_chat_members):
 			ricebot.send_message(chat_id, "<code>All contents/events in this group chat are confidential. Disclosure is prohibited</code>", parse_mode="HTML")
+			ricebot.send_message(chat_id, update.message.new_chat_members.full_name, disable_notification=True)
 		#Check if message is text
 		if (update.message.text):
 			msg_text = update.message.text.lower()
