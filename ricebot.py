ad3b5be7b3200704a6907e1d71f44d5677ade114
riceBox <taerogar@gmail.com>
2021-07-08 22:04:08 +0800

    Added saveupgif to prodGC

--- ricebot.py
+++ ricebot.py
@@ -282,9 +282,9 @@ def testGChandle(update, context):
 						ricebot.send_photo(chat_id, saveuppic, reply_to_message_id=msg_id)
 				else :
 					if (update.message.reply_to_message):
-						ricebot.send_animation(chat_id, saveupgif, reply_to_message_id=reply_msg_id)
+						ricebot.send_animation(chat_id, random.choice(saveupgif), reply_to_message_id=reply_msg_id)
 					else:
-						ricebot.send_animation(chat_id, saveupgif, reply_to_message_id=msg_id)
+						ricebot.send_animation(chat_id, random.choice(saveupgif), reply_to_message_id=msg_id)
 			#sad greetings autoreply
 			elif (any(x in msg_split for x in sadgreet) and "rice" in msg_text):
 				if searchinString(sadgreet, msg_text, searchparam=r"(\S+) @ricecooker"):
@@ -425,11 +425,17 @@ def prodGChandle(update, context):
 			elif ("weed jesus" in msg_text):
 				ricebot.send_animation(chat_id, weedjesusgif)
 			#don't waste money autoreply
-			elif (any(x in msg_text for x in saveupgreet) and random.randint(0,2)):
-				if (update.message.reply_to_message):
-					ricebot.send_photo(chat_id, saveuppic, reply_to_message_id=reply_msg_id)
-				else:
-					ricebot.send_photo(chat_id, saveuppic, reply_to_message_id=msg_id)
+			elif (any(x in msg_text for x in saveupgreet)):
+				if (random.randint(0,2)):
+					if (update.message.reply_to_message):
+						ricebot.send_photo(chat_id, saveuppic, reply_to_message_id=reply_msg_id)
+					else:
+						ricebot.send_photo(chat_id, saveuppic, reply_to_message_id=msg_id)
+				else :
+					if (update.message.reply_to_message):
+						ricebot.send_animation(chat_id, random.choice(saveupgif), reply_to_message_id=reply_msg_id)
+					else:
+						ricebot.send_animation(chat_id, random.choice(saveupgif), reply_to_message_id=msg_id)
 			# #greetings autoreply
 			# elif (user_id == 456128183 and "hi" in msg_split):
 			# 	ricebot.send_message(chat_id, "<code>Negative</code>", parse_mode="HTML", reply_to_message_id=msg_id)
